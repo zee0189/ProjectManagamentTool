a65127ac04b5e999462210966f4ab2109922e1b6
zee <mohammed.k@evivehealth.com>
2019-06-17 15:07:55 +0530

    project Structure

---
+++ src/main/java/com/project/ProjectMangementTool/ProjectMangementToolApplication.java
@@ -0,0 +1,13 @@
+package com.project.ProjectMangementTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMangementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMangementToolApplication.class, args);
+	}
+
+}
